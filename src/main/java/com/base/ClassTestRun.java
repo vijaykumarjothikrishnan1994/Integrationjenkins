bf997678e6bd00604a7dfe8c34c766b37d29d49b
vijaykumar <vijaykumar.js1994@gmail.com>
2021-05-16 11:36:56 +0530

    'code'

---
+++ src/main/java/com/base/ClassTestRun.java
@@ -0,0 +1,15 @@
+package com.base;
+
+import org.testng.annotations.Test;
+
+public class ClassTestRun {
+	
+	       
+	@Test
+	public void MavenTestRun() {
+		
+		
+		System.out.println("Hello this is my test");
+	}
+
+}
